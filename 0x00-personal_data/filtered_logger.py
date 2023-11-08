1f3cb2c9ec7db4964db00aa0e0688ade6d69184b
CzarProCoder <juliusczar36@gmail.com>
2023-11-08 09:07:01 +0300

    Update function to obfuscate a message

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,13 @@
 Returns a message obfuscated
 '''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    ''' Obfuscates a message
+    '''
     for f in fields:
         message = re.sub(rf"{f}=(.*?)\{separator}",
                          f'{f}={redaction}{separator}', message)
