095e00bd79b24fc23632c29e756413e600920ffd
CzarProCoder <juliusczar36@gmail.com>
2023-11-10 11:28:21 +0300

    Add a public method to return the Base64 part in authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,16 @@ class BasicAuth(Auth):
     Inherits from Auth
     Implements basic authentication
     '''
+    def extract_base64_authorization_header(self, \
+                                            authorization_header: str) -> str:
+        '''
+        Return the Base64 part from the Authorization
+        part of the header
+        '''
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return(authorization_header.replace("Basic ", ""))
