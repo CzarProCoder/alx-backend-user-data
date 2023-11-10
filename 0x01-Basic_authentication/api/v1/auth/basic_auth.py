5736b48298558b16083cb28dc573ac1ea8374b42
CzarProCoder <juliusczar36@gmail.com>
2023-11-10 11:46:32 +0300

    Add a method to decode a bse64 object to utf-8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module to define basic auth
 '''
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return(authorization_header.replace("Basic ", ""))
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        '''
+        Convert an base64 encoded object into utf-8
+        '''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return(decoded_bytes.decode('utf-8'))
+        except Exception:
+            return None
