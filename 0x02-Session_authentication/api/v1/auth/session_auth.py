b49571e5aaec1f6bd42e80ad36ee7ce894556d04
CzarProCoder <juliusczar36@gmail.com>
2023-11-14 08:01:37 +0300

    Create an empty session class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+Implements Session Authentication
+'''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    '''
+    Defines the implementation of Session Authentication
+    '''
