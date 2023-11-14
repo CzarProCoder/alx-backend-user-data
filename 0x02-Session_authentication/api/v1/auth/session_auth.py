f5b116b29ade119e89a1af2b45fddc72ab05dcb3
CzarProCoder <juliusczar36@gmail.com>
2023-11-14 08:16:49 +0300

    Implement a method to generate session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,20 @@
 Implements Session Authentication
 '''
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''
     Defines the implementation of Session Authentication
     '''
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        ''' Create a session id based on user id
+        '''
+        if user_id is None:
+            return None
+        if user_id is not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
