3a745d4b2dd2f8f21ef4228d3231489c105e94ed
CzarProCoder <juliusczar36@gmail.com>
2023-11-17 16:07:26 +0300

    Add Shebang to python script

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,31 +3,18 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-'''
-Create a sqlalchemy base class
-'''
-
 Base = declarative_base()
 
 
 class User(Base):
-    '''
-    SQLAlchemnt model to implement a user
-    '''
-    __tablename__ = "users"
+    """
+    User class
+    """
+
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    # def __init__(self, id, email, hashed_password, session_id, reser_token):
-    #     '''
-    #     Initializes an instance of User
-    #     '''
-    #     self.id = id
-    #     self.email = email
-    #     self.hashed_password = hashed_password
-    #     self.session_id = session_id
-    #     self.reset_token = reser_token
