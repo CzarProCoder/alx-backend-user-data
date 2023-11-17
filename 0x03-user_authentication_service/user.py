1bd1f59f03743ea0a7c72435b740c82acac9c33e
CzarProCoder <juliusczar36@gmail.com>
2023-11-17 16:03:54 +0300

    Create a sqlalchemy model user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, INTEGER, VARCHAR
+from sqlalchemy import Column, Integer, String
 
 '''
 Create a sqlalchemy base class
@@ -12,20 +12,20 @@ class User(Base):
     '''
     SQLAlchemnt model to implement a user
     '''
-    __tablename__ = "User"
+    __tablename__ = "users"
 
-    id = Column(INTEGER, primary_key=True)
-    email = Column(VARCHAR(250), nullable=False)
-    hashed_password = Column(VARCHAR(250), nullable=False)
-    session_id = Column(VARCHAR(250))
-    reset_token = Column(VARCHAR(250))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
-    def __init__(self, id, email, hashed_password, session_id, reser_token):
-        '''
-        Initializes an instance of User
-        '''
-        self.id = id
-        self.email = email
-        self.hashed_password = hashed_password
-        self.session_id = session_id
-        self.reset_token = reser_token
+    # def __init__(self, id, email, hashed_password, session_id, reser_token):
+    #     '''
+    #     Initializes an instance of User
+    #     '''
+    #     self.id = id
+    #     self.email = email
+    #     self.hashed_password = hashed_password
+    #     self.session_id = session_id
+    #     self.reset_token = reser_token
