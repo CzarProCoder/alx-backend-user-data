5df39beec28ac91c8a9812cf700913916b93bd78
CzarProCoder <juliusczar36@gmail.com>
2023-11-18 14:26:20 +0300

    Create a function to check the validity of a user for auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,16 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email, password):
+        '''
+        User validation for 
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            password = password.encode('utf-8')
+            if bcrypt.checkpw(password, user.hashed_password):
+                return True
+            return False
+        except NoResultFound:
+            return False
