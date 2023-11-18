b894e0cffacfc21d4071d64616e7631c2681ac02
CzarProCoder <juliusczar36@gmail.com>
2023-11-18 17:55:15 +0300

    Add a logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,13 +55,13 @@ def logout() -> str:
     Logout a user from the system
     '''
     try:
-        session_id = request.cookies.get('session_id')
-        user = AUTH.get_user_from_session_id(session_id)
+        session_id = request.cookie.get("session_id")
+        user = AUTH.get_user_from_session(session_id)
         if user:
             AUTH.destroy_session(user.id)
-            return redirect(url_for('index'))
-        else:
-            abort(403)
+            return redirect("/")
+    except NoResultFound:
+        abort(403)
 
 
 if __name__ == '__main__':
