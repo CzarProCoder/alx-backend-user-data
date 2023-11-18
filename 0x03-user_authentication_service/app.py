82885b6cf58f9cec1a9e1953f7db983a8780176b
CzarProCoder <juliusczar36@gmail.com>
2023-11-18 17:57:54 +0300

    Add a logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,14 +49,12 @@ def login():
         return Response
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> str:
-    '''
-    Logout a user from the system
-    '''
+    """ logout a user"""
     try:
-        session_id = request.cookie.get("session_id")
-        user = AUTH.get_user_from_session(session_id)
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
             return redirect("/")
