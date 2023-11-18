bbabba8a196768c78d626241eebb7197ddd4f737
CzarProCoder <juliusczar36@gmail.com>
2023-11-18 17:04:05 +0300

    Add a method to get a user based on their session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,14 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        '''
+        Check the user based on their session id
+        '''
+        if not session_id:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        if not user:
+            return None
+        return user
