3dd9239af12fbd8deca100edc4e838f25c5b40c5
CzarProCoder <juliusczar36@gmail.com>
2023-11-18 13:31:23 +0300

    Add an end-point to register new users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,14 +3,34 @@
 '''
 Entry point to the application
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def index():
+    '''
+    Main index page
+    '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''
+    End-point that registers a user
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == '__main__':
